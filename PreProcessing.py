a138011f8cef7f70cbe23ac0bb54df7b506a3251
Kai Lukowiak <kailukowiak@pop-os.localdomain>
2019-09-21 21:29:52 -0300

    Added tag parser functionality

---
+++ PreProcessing.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from io import StringIO
+
+def tag_parser(string):
+    with open(string) as f:
+        t = f.read()
+        t = t.split("Points =\n")[1]
+        t = t.replace(" 0.1 1 1 \"Marker\"", "")
+        t = t.replace(";", "")
+#        t = t.replace(" ", "", 1)
+        t = t.replace(" \n", "\n")
+        t = StringIO(t)
+        df = pd.read_csv(t, sep = " ", header=None).loc[:, 1:]
+        df.columns = ["x", "y", "z"]
+        
+    return df
+
+        
+test = tag_parser("../mouse_skulls/data/MouseSkulls/475_landmarks.tag")
+
+print(test)
