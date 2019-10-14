f3dab35232baded59c155703c365e335e8c3041b
Kai Lukowiak <kailukowiak@pop-os.localdomain>
2019-10-13 19:16:11 -0600

    Created setup env

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='3DLabeler',
+    packages=['3DLabeler'],
+    description='Deep Learning tools to annotate 3D images',
+    version='0.1.0',
+    url='https://github.com/3DLables/3DLabeler/',
+    author='3DLabeler',
+    author_email='kailukowiak@gmail.com',
+    keywords=['3D Images','MRI','Annotate', 'Genomics']
+    )
