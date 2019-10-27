29c4908dd70bab82fe1a4d4f3d33b58176b87ad2
Kai Lukowiak <kailukowiak@pop-os.localdomain>
2019-10-27 15:17:53 -0600

    Implemented first draft for cube creation

--- ThreeDLabeler/images.py
+++ ThreeDLabeler/images.py
@@ -6,7 +6,7 @@ class Image:
     def __init__(self, voxels, voxel_size, point_positon):
         self.voxels = voxels
         self.voxel_size = voxel_size
-        self.point_positon = point_positon
+        self.point_positon = point_positon / voxel_size
         self.dimensions = {}
         self.dimensions['x'] = self.voxels.shape[0]
         self.dimensions['y'] = self.voxels.shape[1]
@@ -19,6 +19,27 @@ class Image:
         pass
 
 
+    def cube(self):
+        """Returns a cube image with all dimensions equal to the longest."""
+
+        max_dim = max(self.dimensions, key = self.dimensions.get)
+        max_dim = self.dimensions[max_dim]
+        x_target = (max_dim - self.dimensions['x']) / 2
+        y_target = (max_dim - self.dimensions['y']) / 2
+        z_target = (max_dim - self.dimensions['z']) / 2
+
+        self.voxels = np.pad(self.voxels,
+                            ((int(np.ceil(x_target)), int(np.floor(x_target))),
+                             (int(np.ceil(y_target)), int(np.floor(y_target))),
+                             (int(np.ceil(z_target)), int(np.floor(z_target)))),
+                            'constant',
+                            constant_values=(0))
+        self.point_positon = self.point_positon + [np.ceil(x_target),
+                                                     np.ceil(y_target),
+                                                     np.ceil(z_target)]
+
+        
+
     def scale(self, size):
         # TODO
         pass
@@ -29,3 +50,5 @@ def ratio(dims):
     total = dims.sum()
     
     return dims/total
+
+
--- testing.py
+++ testing.py
@@ -1,8 +1,8 @@
 # %%
-import matplotlib.pyplot as plt
 import nibabel as nib
-from mpl_toolkits.mplot3d import axes3d, Axes3D #<-- Note the capitalization!
 import numpy as np
+import importlib
+
 
 # %%
 
@@ -12,22 +12,28 @@ data = img.get_data()
 #%%
 from ThreeDLabeler import images
 from ThreeDLabeler.PreProcessing import tag_parser
-
+# importlib.reload(ThreeDLabeler.images)
 tags = tag_parser('MouseSkullData/475_landmarks.tag')
 
-test = images.Image(data, 'voxelsize', tags)
+test = images.Image(data, (0.035, 0.035, 0.035), tags)
 
+print(test.point_positon)
+print(test.voxels.shape)
+test.cube()
+print(test.point_positon)
+print(test.voxels.shape)
 
+# ############################
+# # a = np.arange(125)       #
+# # a = a.reshape((5, 5, 5)) #
+# # print(a)                 #
+# #                          #
+# #                          #
+# #                          #
+# # # %%                     #
+# ############################
+# np.pad(a, (1, 3), 'constant', constant_values=(0))
 
-a = np.arange(125)
-a = a.reshape((5, 5, 5))
-print(a)
-
-
-
-# %%
-np.pad(a, (1, 3), 'constant', constant_values=(0))
-
-# %%
+# # %%
 
 #%%
