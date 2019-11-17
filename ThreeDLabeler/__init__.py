6a01ab892b7b28492090a6942621d56386c6f0f4
Kai Lukowiak <kailukowiak@gmail.com>
2019-11-17 14:55:58 -0700

    added mri_plot to init

--- ThreeDLabeler/__init__.py
+++ ThreeDLabeler/__init__.py
@@ -2,3 +2,4 @@ from ThreeDLabeler.preprocessing import package_to_npy
 from ThreeDLabeler.preprocessing import upload_to_gcp
 from ThreeDLabeler.preprocessing import tag_parser
 from ThreeDLabeler.images import Image
+from ThreeDLabeler.plotting import mri_plot
