f8557a0880911aa681b64644c9c444ca5894e49e
Kai Lukowiak <kailukowiak@gmail.com>
2020-02-01 16:55:45 -0700

    New version to help upgrade pip

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name='ThreeDLabeler',
     packages=['ThreeDLabeler'],
     description='Deep Learning tools to annotate 3D images',
-    version='0.1.2',
+    version='0.1.3',
     url='https://github.com/3DLables/3DLabeler/',
     author='3DLabeler',
     author_email='kailukowiak@gmail.com',
-    keywords=['3D Images','MRI','Annotate', 'Genomics']
+    keywords=['3D Images', 'MRI', 'Annotate', 'Genomics']
     )
