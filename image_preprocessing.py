429b1f775dbb4b2a717171b925ea2292932f3047
Kai Lukowiak <kailukowiak@gmail.com>
2020-02-17 09:39:48 -0700

    Improved formatting and update import

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -1,17 +1,16 @@
-import preprocessing as pp 
+from ThreeDLabeler import preprocessing as pp
 import os
-import numpy as np 
+import numpy as np
 SKULL_DIR = r"G:\MouseSkulls"
 file_list = os.listdir(SKULL_DIR)
 
 mnc_files = [i for i in file_list if '.mnc' in i]
 tag_files = [i for i in file_list if '.tag' in i]
-file_names = ['skull_' + i.split('.')[0] for i in mnc_files] # change this one
+file_names = ['skull_' + i.split('.')[0] for i in mnc_files]  # change this one
 
 existing = [i for i in file_list if '.npy' in i]
 existing = [i.split('.npy')[0] for i in existing]
 existing = [i[6:] for i in existing]
-
 
 
 file_names.sort()
@@ -28,14 +27,12 @@ mnc_test = np.array([i.split('.')[0] for i in mnc_files])
 existing = np.array(existing)
 file_names = np.array(file_names)
 
-processed_files = np.isin(tag_test, existing )
+processed_files = np.isin(tag_test, existing)
 
 mnc_files = mnc_files[~processed_files]
 tag_files = tag_files[~processed_files]
 file_names = file_names[~processed_files]
 pp.package_to_npy(SKULL_DIR, mnc_files[3:], tag_files[3:], file_names[3:])
-
-
 
 
 # import nibabel as nib
@@ -44,10 +41,10 @@ pp.package_to_npy(SKULL_DIR, mnc_files[3:], tag_files[3:], file_names[3:])
 # skull = pp.Processor(img.get_data(), img.header.get_zooms(), points)
 
 # print(skull.voxels.shape)
-# test = np.pad(skull.voxels, ((278, 278), (444, 444), (293, 293)), 'constant', constant_values=(0))
+# test = np.pad(skull.voxels, ((278, 278), (444, 444), (293, 293)),
+# 'constant', constant_values=(0))
 # print('done test')
 
 # import nilearn as ni
 # from nilearn import plotting
-# ni.plotting.plot_glass_brain("MouseSkullData/test.nii")   
-
+# ni.plotting.plot_glass_brain("MouseSkullData/test.nii")
